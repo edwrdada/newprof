eacd1d70f60d50ff1062a85abbfceed45f3c3f00
Nao Ada <0911adachi@gmail.com>
2019-03-06 15:49:11 +0900

    create all controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(user).permit()
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'login/login'
+  get 'homes/home'
+  get 'homes/about'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/new'
+  root 'homes#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
