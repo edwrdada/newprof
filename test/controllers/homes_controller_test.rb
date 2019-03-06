3627b2fee02e60d4ca6183235d31ea226f914bbd
Nao Ada <0911adachi@gmail.com>
2019-03-06 17:28:03 +0900

    bcrypt and test to gemfile

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -4,6 +4,8 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get homes_home_url
     assert_response :success
+    assert_template 'home.html.erb'
+  
   end
 
 end
