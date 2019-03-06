aeb4a3e456a08076f2957e07afa507dffc095437
Nao Ada <0911adachi@gmail.com>
2019-03-06 16:09:08 +0900

    create model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(user).permit()
+    params.require(user).permit(:name, :email, :profile)
   end
 end
